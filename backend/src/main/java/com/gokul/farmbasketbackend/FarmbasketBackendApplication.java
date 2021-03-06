3eb161d4a93f878b94bde549f95261466d598c20
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2021-03-06 12:09:39 +0100

    initial commit

---
+++ backend/src/main/java/com/gokul/farmbasketbackend/FarmbasketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.farmbasketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmbasketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmbasketBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gokul/farmbasketbackend/FarmbasketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.farmbasketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmbasketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
